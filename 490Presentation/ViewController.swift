357346436fa1b667396508f3cb2b8d3e7386f513
vietbui99 <buiquocviet99@gmail.com>
2023-03-06 22:59:34 -0600

    added files for presentation

--- 490Presentation/ViewController.swift
+++ 490Presentation/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var result : Int!
+    var firstNum : Int!
+    var secondNum : Int!
+    
+    @IBOutlet weak var resultLabel: UILabel!
 
+    @IBOutlet weak var firstNumTextField: UITextField!
+    
+    @IBOutlet weak var secondNumTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func addNumberButton(_ sender: Any) {
+        firstNum = Int(firstNumTextField.text!)
+        secondNum = Int(secondNumTextField.text!)
+        result = firstNum + secondNum
+        resultLabel.text = String(result)
+    }
 }
 
